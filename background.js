67d1ada4ad8c3bf723f51fbaec232938a19236b2
avidrucker <avidrucker@gmail.com>
2024-03-25 22:12:28 -0400

    fix XMLHttpRequest error

--- background.js
+++ background.js
@@ -2,19 +2,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "refreshEmail") {
       // Authenticate the user and get an access token
       chrome.identity.getAuthToken({interactive: true}, function(token) {
-        // Create a new XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://www.googleapis.com/gmail/v1/users/me/messages?q=in:inbox&maxResults=1');
-        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-        xhr.onload = function() {
-          // Parse the response
-          var response = JSON.parse(xhr.responseText);
+        // Use the fetch API to make the request
+        fetch('https://www.googleapis.com/gmail/v1/users/me/messages?q=in:inbox&maxResults=1', {
+          headers: {
+            'Authorization': 'Bearer ' + token
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
           // Get the first email
-          var email = response.messages[0];
+          var email = data.messages[0];
           // Send the email back to the popup script
           sendResponse({email: email});
-        };
-        xhr.send();
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
       });
       return true; // indicates that the response will be sent asynchronously
     }
