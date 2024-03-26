debc610e8b9d685ce7796d698792f74745ebac87
avidrucker <avidrucker@gmail.com>
2024-03-26 09:52:20 -0400

    fix background.js to handle email id

--- background.js
+++ background.js
@@ -8,15 +8,40 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             'Authorization': 'Bearer ' + token
           }
         })
-        .then(response => response.json())
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Gmail API request failed with status ' + response.status);
+          }
+          return response.json();
+        })
         .then(data => {
-          // Get the first email
-          var email = data.messages[0];
+          // Check if the response has the expected structure
+          if (!data.messages || data.messages.length === 0) {
+            throw new Error('Unexpected response from the Gmail API');
+          }
+          // Get the ID of the first email
+          var emailId = data.messages[0].id;
+          // Get the email
+          return fetch('https://www.googleapis.com/gmail/v1/users/me/messages/' + emailId + '?format=raw', {
+            headers: {
+              'Authorization': 'Bearer ' + token
+            }
+          });
+        })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Gmail API request failed with status ' + response.status);
+          }
+          return response.json();
+        })
+        .then(email => {
           // Send the email back to the popup script
           sendResponse({email: email});
         })
         .catch(error => {
           console.error('Error:', error);
+          // Send an error message back to the popup script
+          sendResponse({error: error.message});
         });
       });
       return true; // indicates that the response will be sent asynchronously
